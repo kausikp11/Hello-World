16d2573c9805f5d38d801388b1e7b2e339451e21
kausikp11 <kausikp11@gmail.com>
2019-09-17 22:22:48 +0530

    Add files

---
+++ Hello_World_C++.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world";//cout is the command used to print the text and variable in between the << and ;
+    return 0;
+}
---
+++ Hello_World_Java.java
@@ -0,0 +1,8 @@
+package com.kausik;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");//System.out.println used to print the text or variable inside the bracket
+    }
+}
